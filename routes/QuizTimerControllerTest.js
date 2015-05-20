8496954253bf3bf2c0d5197e4513dd8e6d14a4f2
Nabil <himnabil@gmail.com>
2015-05-20 16:17:46 +0200

    compleat V1

--- routes/QuizTimerControllerTest.js
+++ routes/QuizTimerControllerTest.js
@@ -5,13 +5,27 @@
 var Timer = require('../services/QuizTimer.js');
 var io;
 
+var quiz = {title : 'lol' , questions : [ " q1  ", " q2" , "q3" , "q4" ]};
+var room = { id : 10 , currentQ : -1 , timeoutQ : 1000 , quiz : quiz  };
 
+var quizeTimer = new Timer.Manager(room);
 
+quizeTimer.onBeginOfQuestion= function ( question , room ){
+    console.log( "begin  " + question  );
+};
 
+quizeTimer.onEndOfQuestion= function ( question , room ){
+    console.log( "ending  " + question  );
+};
+
+quizeTimer.onEndOfQuiz= function ( room ){
+    console.log( "ending the Quiz "  );
+};
 // public
 
 exports.setIO = function( _io){ io = _io;};
 
 exports.test = function(req, res){
     res.render('QuizTimerTestView', { title: 'Quiz Timer Test' });
+    quizeTimer.start();
 };
--- services/QuizTimer.js
+++ services/QuizTimer.js
@@ -1,5 +1,32 @@
 /**
  * Created by Nabil on 5/20/2015.
  */
+function callBack (quizTimer){
+    quizTimer.onEndOfQuestion(quizTimer.room.quiz.questions[quizTimer.room.currentQ] , quizTimer.room);
+    quizTimer.room.currentQ++;
+    if (quizTimer.room.currentQ < quizTimer.room.quiz.questions.length)
+    {
+        quizTimer.onBeginOfQuestion(quizTimer.room.quiz.questions[quizTimer.room.currentQ] , quizTimer.room);
+        setTimeout(callBack, quizTimer.room.timeoutQ ,quizTimer );
+    }else
+    {
+        quizTimer.onEndOfQuiz( quizTimer.room);
+    }
+}
 
 
+
+function QuizTimer( room ){
+    this.room = room ;
+    this.onEndOfQuestion = function ( question , room ){};
+    this.onBeginOfQuestion = function ( question , room ){};
+    this.onEndOfQuiz = function ( room ){};
+
+    this.start = function (){
+        console.log ('the Quiz'+this.room.quiz.title+' is stating in room '+ this.room.id);
+        this.room.currentQ = 0;
+        this.onBeginOfQuestion(this.room.quiz.questions[this.room.currentQ] , this.room);
+        setTimeout( callBack, this.room.timeoutQ , this );
+    };
+}
+exports.Manager = QuizTimer ;
