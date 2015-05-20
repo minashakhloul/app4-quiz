3a165c40dc76a5bb2c0a45a12d437d7568c4940d
Nabil <himnabil@gmail.com>
2015-05-20 12:18:36 +0200

    1st Step

---
+++ appTest.js
@@ -0,0 +1,44 @@
+/**
+ * Created by Nabil on 5/20/2015.
+ */
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+    , routes = require('./routes');
+
+var lobbyController = require('./routes/lobby-controller')
+var QuizTimerControlerTest = require('./routes/QuizTimerControllerTest')
+//var app = module.exports = express.createServer();
+var app = express.createServer();
+var io = require('socket.io')(app);
+app.configure(function(){
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+    app.use(express.errorHandler());
+});
+
+// *********  Routes ********* //
+app.get('/', routes.index);
+app.get('/poke', routes.poke);
+app.get('/peek', routes.peek);
+app.get('/roomlist', lobbyController.roomlist);
+app.get('/quizTimerTest',QuizTimerControlerTest.test);
+
+// *********  Launch ********* //
+
+app.listen(3000, function(){
+    console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
