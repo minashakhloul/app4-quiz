9d35ebffbd278b96727aac000433921ff3bb62fb
Nabil <himnabil@gmail.com>
2015-05-21 23:10:54 +0200

    set up test dev quiz-eval.js

---
+++ routes/quiz-evalTest.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Nabil on 5/21/2015.
+ */
+
+
+quizEval = require("../services/quiz-eval");
+
+
+
+
+
+
+
+
+
+
+
+
+exports.test = function(req, res){
+    res.render('onConsoleTestView', { title: 'Quiz Timer Test' });
+
+};
---
+++ services/quiz-eval.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Nabil on 5/21/2015.
+ */
+
+
+
+function _evaluateur( room  ){
+    this.room = room ;
+    this.score = 0;
+    /*
+    * answersIndexes est un tableau de boolean contient pour chaque propsition si elle est sectioné ou pas
+    * */
+    this.evaluateCurrentQuestion = function ( prop_selectionState  ){
+
+        currentQestion = this.room.quiz.questions[this.room.currentQ];
+        if (currentQestion.prop.length != prop_selectionState.length) throw Error("incorrect prop_selectionState.length ");
+
+        var evaluation = true ;
+        for (var i = 0 ; i<prop_selectionState.length  && evaluation ; i++){
+            if (currentQestion.prop[i] != prop_selectionState[i]) evaluation = false ;
+        }
+
+        if (evaluation) this.score ++ ;
+    };
+
+} // index
+
+exports.evaluateur = _evaluateur;
+
